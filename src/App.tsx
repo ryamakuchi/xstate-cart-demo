6609e48c78e5ec6286c5e9ebc2de30231a66ec5c
rry <r.yamakuchi@gmail.com>
2021-12-12 13:20:14 +0900

    feat: Chakra UI の設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './assets/logo.svg'
+import './assets/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
